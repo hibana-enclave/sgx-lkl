74a50024c301cf956a5ddb07e7a41602368c872a
randoruf <masswie22@gmail.com>
2023-05-21 11:23:03 +0800

    host interface

--- src/host_interface/host_interface_calls.c
+++ src/host_interface/host_interface_calls.c
@@ -112,14 +112,14 @@ int sgxlkl_host_syscall_mprotect(void* addr, size_t len, int prot)
 void sgxlkl_host_app_main_end(void)
 {
     sgx_lkl_aex_cnt = __sgx_lkl_aex_cnt_aux; 
-    __sgx_step_app_terminated = 1; 
+    __sgx_step_app_terminated = 1;
     printf("[[ ENC ]] ************** Application End   **************\n");
 } 
 
 void sgxlkl_host_app_main_start(void)
 {
-    __sgx_step_app_terminated = 0; 
-    __sgx_lkl_aex_cnt_aux = 0; 
+    __sgx_step_app_terminated = 0;
+    __sgx_lkl_aex_cnt_aux = 0;
     printf("[[ ENC ]] ************** Application Start **************\n");
 }
 
