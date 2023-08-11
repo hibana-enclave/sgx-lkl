a54c653d11c5fff0a77f773c59baa499c149e8c5
randoruf <masswie22@gmail.com>
2023-08-11 23:53:44 +0800

    sgxstep attack delay

--- src/include/host/sgxlkl_params.h
+++ src/include/host/sgxlkl_params.h
@@ -47,6 +47,7 @@
 #define SGXLKL_ENABLE_SWIOTLB "SGXLKL_ENABLE_SWIOTLB"
 #define SGXLKL_HD_OVERLAY "SGXLKL_HD_OVERLAY"
 #define SGXLKL_HOST_IMPORT_ENV "SGXLKL_HOST_IMPORT_ENV"
+#define SGXSTEP_ATTACK_DELAY "SGXSTEP_ATTACK_DELAY"
 
 #ifndef SGXLKL_RELEASE
 /* These environment variables do not have config settings, they are
