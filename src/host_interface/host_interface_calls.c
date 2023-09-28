55a89fe61bb1eaa307c9e74a9fb0a93e762cc5e1
randoruf <masswie22@gmail.com>
2023-09-28 18:15:16 +0800

    remove

--- src/host_interface/host_interface_calls.c
+++ src/host_interface/host_interface_calls.c
@@ -144,20 +144,20 @@ void sgxlkl_host_app_main_start(void)
 
 void sgxlkl_host_sgx_step_attack_setup(void)
 {
-    __aex_count = 0; 
-    if (__sgx_step_apic_triggered != STEP_PHASE_0){
-        sgxlkl_host_fail("Don't issue ud2 more thane once...."); 
-    }
-    __sgx_step_apic_triggered = STEP_PHASE_1; 
-    // sgxlkl_host_info("Establishing user-space APIC/IDT mappings at CPU %d...", sched_getcpu()); 
-    idt_t idt = {0};
-    map_idt(&idt);
-    install_kernel_irq_handler(&idt, __ss_irq_handler, IRQ_VECTOR); // FIXME: the installation of kernel irq handler may freeze the kernel?
-    srand(time(NULL)); 
-    const uint64_t delay_time = ATTACK_TIMER_BASE_TIME + rand() % ATTACK_TIMER_RANGE; 
-    sgxlkl_host_info("[[ SGX-STEP ]] attacks will start after %llu cpu cycles...", (unsigned long long)delay_time); 
-    apic_timer_oneshot(IRQ_VECTOR);
-    apic_timer_irq((unsigned long long)delay_time); 
+    // __aex_count = 0; 
+    // if (__sgx_step_apic_triggered != STEP_PHASE_0){
+    //     sgxlkl_host_fail("Don't issue ud2 more thane once...."); 
+    // }
+    // __sgx_step_apic_triggered = STEP_PHASE_1; 
+    // // sgxlkl_host_info("Establishing user-space APIC/IDT mappings at CPU %d...", sched_getcpu()); 
+    // idt_t idt = {0};
+    // map_idt(&idt);
+    // install_kernel_irq_handler(&idt, __ss_irq_handler, IRQ_VECTOR); // FIXME: the installation of kernel irq handler may freeze the kernel?
+    // srand(time(NULL)); 
+    // const uint64_t delay_time = ATTACK_TIMER_BASE_TIME + rand() % ATTACK_TIMER_RANGE; 
+    // sgxlkl_host_info("[[ SGX-STEP ]] attacks will start after %llu cpu cycles...", (unsigned long long)delay_time); 
+    // apic_timer_oneshot(IRQ_VECTOR);
+    // apic_timer_irq((unsigned long long)delay_time); 
 }
 
 void sgxlkl_host_hw_cpuid(
